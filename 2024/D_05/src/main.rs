d5e79bafebecda1d0ca0931ad09b8addef1b6cd8
Mirko Sacchettti <ms@random.cards>
2024-12-07 21:11:48 +0100

    day 5

--- 2024/D_05/src/main.rs
+++ 2024/D_05/src/main.rs
@@ -1,64 +1,85 @@
-use regex::Regex;
 use std::fs;
 
+struct PrintQueue {
+    rules: Vec<(i32, i32)>,
+    updates: Vec<Vec<i32>>,
+}
+
 fn main() {
-    let muls = parse_input("input.txt");
-    println!("part one:{}", part_one(&muls));
-    println!("part two:{}", part_two(&muls));
+    let q = parse_input("input.txt");
+    println!("part one:{}", part_one(&q));
+    println!("part two:{}", part_two(&q));
 }
 
-fn parse_input(filename: &str) -> String {
-    fs::read_to_string(filename).expect("Impossibile leggere il file")
+fn parse_input(filename: &str) -> PrintQueue {
+    let content = fs::read_to_string(filename).expect("Impossibile leggere il file");
+    let mut updates = Vec::new();
+    let mut rules = Vec::new();
+    for line in &mut content.lines() {
+        if line.trim().is_empty() {
+            continue;
+        }
+        let parts: Vec<&str> = line.split('|').collect();
+        if parts.len() == 2 {
+            let a = parts[0].trim().parse::<i32>().expect("Numero non valido");
+            let b = parts[1].trim().parse::<i32>().expect("Numero non valido");
+            rules.push((a, b));
+        } else {
+            let rule: Vec<i32> = line
+                .split(',')
+                .map(|s| s.trim().parse::<i32>().expect("Numero non valido"))
+                .collect();
+            updates.push(rule);
+        }
+    }
+    PrintQueue { updates, rules }
 }
 
-fn part_one(memory: &str) -> i32 {
-    let pattern = r"mul\((\d{1,3}),(\d{1,3})\)";
-    let re = Regex::new(pattern).unwrap();
-    re.captures_iter(memory)
-        .map(|cap| {
-            let x: i32 = cap[1].parse().unwrap();
-            let y: i32 = cap[2].parse().unwrap();
-            x * y
-        })
-        .sum()
+fn part_one(pq: &PrintQueue) -> i32 {
+    let mut correctly_ordered = 0;
+    for update in &pq.updates {
+        if validate(&pq.rules, update) {
+            correctly_ordered += update[update.len() / 2]
+        }
+    }
+    correctly_ordered
 }
 
-fn part_two(memory: &str) -> i32 {
-    let mul_re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let do_re = Regex::new(r"do\(\)").unwrap();
-    let dont_re = Regex::new(r"don't\(\)").unwrap();
+fn part_two(pq: &PrintQueue) -> i32 {
+    let mut correctly_ordered = 0;
+    for update in &pq.updates {
+        if !validate(&pq.rules, update) {
+            let fixed = fixing(&pq.rules, update.clone());
+            correctly_ordered += fixed[fixed.len() / 2]
+        }
+    }
+    correctly_ordered
+}
 
-    let mut sum = 0;
-    let mut pos = 0;
-    let mut enabled = true;
-
-    while pos < memory.len() {
-        if let Some(m) = do_re.find(&memory[pos..]) {
-            if m.start() == 0 {
-                enabled = true;
-                pos += m.end();
-                continue;
+fn validate(rules: &[(i32, i32)], update: &[i32]) -> bool {
+    for &(a, b) in rules {
+        let pos_a = update.iter().position(|&x| x == a);
+        let pos_b = update.iter().position(|&x| x == b);
+        if let (Some(pos_a), Some(pos_b)) = (pos_a, pos_b) {
+            if pos_a > pos_b {
+                return false;
             }
         }
-        if let Some(m) = dont_re.find(&memory[pos..]) {
-            if m.start() == 0 {
-                enabled = false;
-                pos += m.end();
-                continue;
-            }
-        }
-        if enabled {
-            if let Some(cap) = mul_re.captures(&memory[pos..]) {
-                if cap.get(0).unwrap().start() == 0 {
-                    let n1: i32 = cap[1].parse().unwrap();
-                    let n2: i32 = cap[2].parse().unwrap();
-                    sum += n1 * n2;
-                    pos += cap.get(0).unwrap().end();
-                    continue;
+    }
+    true
+}
+
+fn fixing(rules: &[(i32, i32)], mut update: Vec<i32>) -> Vec<i32> {
+    while !validate(rules, &update) {
+        for &(a, b) in rules {
+            let pos_a = update.iter().position(|&x| x == a);
+            let pos_b = update.iter().position(|&x| x == b);
+            if let (Some(pos_a), Some(pos_b)) = (pos_a, pos_b) {
+                if pos_a > pos_b {
+                    update.swap(pos_a, pos_b);
                 }
             }
         }
-        pos += 1;
     }
-    sum
+    update.to_vec()
 }
